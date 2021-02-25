40308f18d79e4b904290d351c098eb4c8d99b002
Yoruet <59109857+Yoruet@users.noreply.github.com>
2021-02-25 22:50:46 +0800

    Update ChangeFileName.py

--- FileName/ChangeFileName.py
+++ FileName/ChangeFileName.py
@@ -18,18 +18,18 @@ def getFileName(t_path):
     for i in f_list:
         for j in vedio_ext:
             if os.path.splitext(i)[1] == j:
-                name = re.sub(r'(?<=\[)[0-9]+(?=\])','$$--$$',os.path.splitext(i)[0])
+                name = re.sub(r'(?<=\[)[0-9]+(?=\])','$$--$$',os.path.splitext(i)[0]) #视频名格式
                 return name + ass_ext
             
 if __name__ == '__main__':
     f_list = getFileList(path)
     name = getFileName(path)
     for i in f_list:
-        index = re.findall(r'(?<=\[)[0-9]+(?=\])',i)
+        index = re.findall(r'(?<=\[)[0-9]+(?=\])',i) #字幕名格式
         oldname = path + i
-        index = re.findall(r'(?<=\[)[0-9]+(?=\])',i)
+        index = re.findall(r'(?<=\[)[0-9]+(?=\])',i) #字幕名格式
         if len(index) == 0:
             continue
-        newname = path + re.sub(r'(?<=\[)\$\$--\$\$(?=\])', index[-1], name)
+        newname = path + re.sub(r'(?<=\[)\$\$--\$\$(?=\])', index[-1], name) #视频名格式
         print('old:',oldname,'=====>',"new:",newname,'\n')
         os.rename(oldname,newname)
